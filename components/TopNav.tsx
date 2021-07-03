9c835884832634d86b9b61307a87b583d24704ce
Tai Chen Li <dev@taichen.li>
2021-07-03 15:35:44 -0400

    ajustment to topnav

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function TopNav() {
   return (
-    <div className="text-purple-500">
+    <div className="bg-gray-600 overflow-hidden h-20 w-screen">
       <p>This is the best thing ever</p>
     </div>
   );
