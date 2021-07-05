e51407405a5c2499159689cdbce82c1f09b750ae
Li Tai Chen <dev@taichen.li>
2021-07-04 23:04:59 -0400

    continuing on laptop

--- components/MainLandingBody.tsx
+++ components/MainLandingBody.tsx
@@ -2,12 +2,14 @@ function MainLandingBody() {
     return (
         <div className="absolute h-4/5 flex flex-col w-full bg-gradient-to-r from-green-400 to-blue-500">
             <div className="flex flex-row flex-grow">
-                <p className="p-10 text-6xl self-center">
+                <p className="p-10 w-2/5 text-6xl self-center">
                     Investment for all
                 </p>
-                <a className="flex-grow"></a>
-                <div className="p-10 w-96 h-96">
-                    ff
+                <div className="p-10 flex-grow">
+                    <div className="flex flex-col">
+                        <p>Invest in large scale project </p>
+                        <p>Hi</p>
+                    </div>
                 </div>
             </div>
         </div>
