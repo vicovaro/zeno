ff2a185506e75d803dde4ca42a7d84fc3663c3dc
Li Tai Chen <dev@taichen.li>
2021-07-05 19:12:18 -0400

    cotninue on laptop

--- components/MainLandingBody.tsx
+++ components/MainLandingBody.tsx
@@ -1,14 +1,20 @@
+import Image from 'next/image'
+
+import realEstateSample from '../public/joel-filipe-RFDP7_80v5A-unsplash.jpg'
+
 function MainLandingBody() {
     return (
         <div className="absolute h-4/5 flex flex-col w-full bg-gradient-to-r from-green-400 to-blue-500">
             <div className="flex flex-row flex-grow">
-                <p className="p-10 w-2/5 text-6xl self-center">
-                    Investment for all
+                <p className="p-10 w-2/5 text-6xl font-bold self-center">
+                    Invest big for all
                 </p>
                 <div className="p-10 flex-grow">
                     <div className="flex flex-col">
-                        <p>Invest in large scale project </p>
-                        <p>Hi</p>
+                        <a className="w-96 h-80 bg-black self-center">
+                            <Image width="500" height="400" src={realEstateSample} alt="Sample of a real estate to invest in" />
+                        </a>
+
                     </div>
                 </div>
             </div>
