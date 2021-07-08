9a7b1a3326097ef49926b51fa9cbb957155bff73
Tai Chen Li <dev@taichen.li>
2021-07-08 17:06:19 -0400

    starting to work on index better

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,18 +7,17 @@ import LandingPageBody1 from "@components/LandingPageBody1";
 import LandingPageBody2 from "@components/LandingPageBody2";
 import Footer from "@components/Footer";
 
-
 export default function Home() {
   return (
     <div className="flex justify-between flex-col content-start	w-full">
       <Head>
         <title>Zeno</title>
-        <meta name="viewport"/>
+        <meta name="viewport" />
       </Head>
-        <TopNav></TopNav>
-        <LandingPageBody1></LandingPageBody1>
-        <LandingPageBody2></LandingPageBody2>
-        <Footer></Footer>
+      <TopNav></TopNav>
+      <LandingPageBody1></LandingPageBody1>
+      <LandingPageBody2></LandingPageBody2>
+      <Footer></Footer>
     </div>
   );
 }
