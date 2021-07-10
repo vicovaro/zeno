672c18a029d0a116948f6ee27001513a5346d57e
Li Tai Chen <dev@taichen.li>
2021-07-10 14:56:21 -0400

    redesigned top bar done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/solid";
 
 function Header() {
   const [isFeatured, setFeatured] = useState(false);
@@ -11,18 +12,32 @@ function Header() {
         {/* Brand Logo */}
         <div className="font-bold text-4xl md:pl-6 pr-12">Zeno</div>
         {/* Top navigation options that stays on the left */}
+
         <div
-          className="flex"
+          className="flex md:pr-6"
           onMouseEnter={() => setFeatured(true)}
           onMouseLeave={() => setFeatured(false)}
         >
           {/* Featured button for hand picked projects */}
-          <button className="font-semibold md:pr-6 hover:text-blue-700">
-            Featured
-          </button>
+          <div className="flex flex-row">
+            <button className="align-middle font-semibold transition duration-500 ease-in-out hover:text-blue-700">
+              Featured
+            </button>
+            {isFeatured ? (
+              <ChevronUpIcon
+                className="my-auto h-3 w-3 text-gray-700 fill-current"
+                aria-hidden="true"
+              />
+            ) : (
+              <ChevronDownIcon
+                className="my-auto h-3 w-3 text-gray-500 fill-current"
+                aria-hidden="true"
+              />
+            )}
+          </div>
           {/* Show on hover */}
           {isFeatured && (
-            <ul className="absolute top-12 p-20 bg-blue-500 rounded-xl z-10">
+            <ul className="absolute top-14 p-4 bg-blue-500 rounded z-10">
               <li>Coffee</li>
               <li>Tea</li>
               <li>Coca Cola</li>
@@ -39,7 +54,7 @@ function Header() {
         {/* Empty div for resizing */}
         <div className="flex-grow"></div>
         {/* Log In and Sign Up button duo */}
-        <button className="font-semibold py-2 px-4 rounded transition duration-500 ease-in-out md:mr-6 hover:text-blue-700 hover:shadow">
+        <button className="font-semibold py-2 px-4 transition duration-500 ease-in-out md:mr-6 hover:text-blue-700">
           Login
         </button>
         <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded transition duration-500 ease-in-out md:mr-4 hover:bg-blue-700">
