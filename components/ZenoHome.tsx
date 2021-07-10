d345863d804e2338c51e4741cc0f1862f65bbcd4
Li Tai Chen <dev@taichen.li>
2021-07-10 18:13:10 -0400

    basic code for background image

--- components/ZenoHome.tsx
+++ components/ZenoHome.tsx
@@ -1,5 +1,22 @@
+import Image from 'next/image'
+import landingPageImage from '@public/joel-filipe-RFDP7_80v5A-unsplash.jpg'
+
 function ZenoHome() {
-  return <div className="relative">ZenoHome</div>;
+  return <div className="relative">
+    {/* Background image */}
+    <div className="fixed overflow-hidden w-screen h-screen">
+      <Image
+        alt="Social housing"
+        src={landingPageImage}
+        layout="fill"
+        objectFit="cover"
+        quality={100}></Image>
+    </div>
+    <div className="text-black z-10">
+      this is good
+    </div>
+    <div className="text-black z-10">May I know what's going on</div>
+  </div>;
 }
 
 export default ZenoHome;
