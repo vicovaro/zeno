5902c8b9c86927e056fbc3eb8c21d8d825f8846e
Li Tai Chen <dev@taichen.li>
2021-07-09 23:34:46 -0400

    top bar starting to look like something

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,12 +7,16 @@ function Header() {
         {/* Brand Logo */}
         <div className="font-bold text-4xl md:pl-6">Zeno</div>
         {/* Top navigation options that stays on the left */}
-        <button className="font-semibold">Projects</button>
-        <button className="font-semibold">Contacts</button>
+        <button className="font-semibold md:pl-12 hover:text-blue-700">
+          Projects
+        </button>
+        <button className="font-semibold md:pl-6 hover:text-blue-700">
+          Contacts
+        </button>
         {/* Empty div for resizing */}
         <div className="flex-grow"></div>
         {/* Log In and Sign Up button duo */}
-        <button className="font-semibold py-2 px-4 rounded transition duration-500 ease-in-out md:mr-4 hover:bg-gray-400">
+        <button className="font-semibold py-2 px-4 rounded transition duration-500 ease-in-out md:mr-6 hover:text-blue-700 hover:shadow">
           Login
         </button>
         <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded transition duration-500 ease-in-out md:mr-4 hover:bg-blue-700">
